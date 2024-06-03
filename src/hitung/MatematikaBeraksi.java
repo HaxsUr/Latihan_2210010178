4d3cd1fbb5e33ada828b145e5e0f4b36f84f0d5e
HaxsUr <m4d4nhaja@gmail.com>
2024-06-03 11:26:02 +0800

    Membuat Class Matematika Beraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -2,5 +2,14 @@
 package hitung;
 
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        //Membuat Objek
+        Matematika madan = new Matematika(7,8);
+        
+        System.out.println("Hasil Penjumlahan : " + madan.setPenjumlahan());
+        System.out.println("Hasil Pengurangan : " + madan.setPengurangan()); 
+        System.out.println("Hasil Perkalian : " + madan.setPerkalian()); 
+        System.out.println("Hasil Pembagian : " + madan.setPembagian()); 
+        
+    }
 }
